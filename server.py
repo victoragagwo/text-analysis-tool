deac49b9025a811ba4ca81f05e023ff3582b31f3
victoragagwo <victoragagwo100@gmail.com>
2026-01-08 10:55:30 +0100

    server issue

--- server.py
+++ server.py
@@ -47,4 +47,6 @@ def getQuotes():
         abort(500, "Failed to fetch quotes from external API")
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
